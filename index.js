61a63ca10d8ef2f8e21dc2a93079929d3f3ab0a5
sukanyagovindan <gsukanya@gmail.com>
2022-06-20 10:54:15 -0700

    submit assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function mondayWork(someString = "go to the office") {
+  
+        
+            return "This Monday, I will " + someString + ".";
+        
+    
+}
+
+function saturdayFun(someString = "roller-skate") {
+    return "This Saturday, I want to " + someString + "!";
+}
+
+function wrapAdjective(visualFlair = "*") {
+    return function (someString = "special") {
+        return "You are " + visualFlair + someString + visualFlair + "!";
+    }
+}
